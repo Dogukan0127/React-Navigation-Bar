eb482ea5e5a10bced5df47cfdb71f54c8184e2e2
Dogukan0127 <dogukangs2005@gmail.com>
2021-07-29 18:58:32 +0300

    component add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import './Components/Logo/Logo.jsx';
-import Logo from './Components/Logo/Logo.jsx';
-import './Components/Menu/Menu.jsx';
-import Menu from './Components/Menu/Menu.jsx';
+import NavBar from './Components/NavBar/navbar';
 
 function App() {
   return (
-    <div className="Nav-container">
-      <div className="Nav-Items">
-
-        <Logo />
-        <Menu />
-
-      </div>
-    </div>
-  );
+    <NavBar />
+  )
 }
 
 export default App;
---
+++ src/Components/NavBar/navbar.jsx
@@ -0,0 +1,23 @@
+
+export default function NavBar() {
+    return (
+        <div className="Nav-container">
+            <div className="Nav-Items">
+
+                <div className="Logo">
+                    <p><a href="#">Logo</a></p>
+                </div>
+
+                <div className="Menu">
+                    <ul>
+                        <li><a href="#">Search</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">HomePage</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    )
+}
